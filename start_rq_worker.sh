a5fc63497eb4b5f72e99d88d496e1dd5c19d66c4
Mike Wilson <geekinutah@gmail.com>
2018-11-05 14:48:19 -0700

    Use pip3 instead of pip

--- start_rq_worker.sh
+++ start_rq_worker.sh
@@ -4,14 +4,14 @@
 
 if [ ${PIP_PACKAGES} != 'none' ]; then
     for i in $(echo ${PIP_PACKAGES} | sed 's/,/ /g'); do
-        pip install $i
+        pip3 install $i
     done
 fi
 
 # If there is a requirements file, install that
 
 if [ ${PIP_REQUIREMENTS} != 'none' ]; then
-    pip install -r ${PIP_REQUIREMENTS}
+    pip3 install -r ${PIP_REQUIREMENTS}
 fi
 
 cat /etc/supervisor/conf.d/rqworker.conf.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /etc/supervisor/conf.d/rqworker.conf
